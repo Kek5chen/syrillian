3c74c4d73051e75f4fd8c5d0319249295c12f673
Kek5chen <52585984+Kek5chen@users.noreply.github.com>
2025-04-21 19:45:17 +0200

    fix: make drawable functions optional

--- src/drawables/drawable.rs
+++ src/drawables/drawable.rs
@@ -6,19 +6,20 @@ use wgpu::{Device, Queue, RenderPass};
 use crate::object::GameObjectId;
 use crate::world::World;
 
+#[allow(unused_variables)]
 pub trait Drawable: Any {
     fn setup(
         &mut self,
         device: &Device,
         queue: &Queue,
         world: &mut World,
-    );
+    ) {}
     fn update(
         &mut self,
         world: &mut World,
         parent: GameObjectId,
         queue: &Queue,
         outer_transform: &Matrix4<f32>,
-    );
+    ) {}
     fn draw(&self, world: &mut World, rpass: &mut RenderPass);
 }
