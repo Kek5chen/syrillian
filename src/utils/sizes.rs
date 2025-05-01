e11a3468aa0cacacbfb0bbe9682e993aa3d1c107
Kek5chen <52585984+Kek5chen@users.noreply.github.com>
2025-05-01 17:51:25 +0200

    chore: clean up const checks

--- src/utils/sizes.rs
+++ src/utils/sizes.rs
@@ -1,8 +1,13 @@
 use nalgebra::{Vector2, Vector3, Vector4};
+use static_assertions::const_assert_eq;
 
 pub const VEC2_SIZE: usize = size_of::<Vector2<f32>>();
 pub const VEC3_SIZE: usize = size_of::<Vector3<f32>>();
 pub const VEC4_SIZE: usize = size_of::<Vector4<f32>>();
+
+const_assert_eq!(size_of::<Vector2<f32>>(), VEC2_SIZE);
+const_assert_eq!(size_of::<Vector3<f32>>(), VEC3_SIZE);
+const_assert_eq!(size_of::<Vector4<f32>>(), VEC4_SIZE);
 
 pub const WGPU_VEC2_SIZE: usize = 8;
 pub const WGPU_VEC3_SIZE: usize = 16;
