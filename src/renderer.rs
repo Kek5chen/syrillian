372e44f97a9772d415a35599e0fbb634bafd7fdf
Kek5chen <52585984+Kek5chen@users.noreply.github.com>
2025-05-01 17:49:42 +0200

    refactor: move error into string

--- src/renderer.rs
+++ src/renderer.rs
@@ -254,7 +254,7 @@ impl Renderer {
                 return false;
             }
             Err(e) => {
-                error!("Surface error: {}", e);
+                error!("Surface error: {e}");
                 return false;
             }
         };
