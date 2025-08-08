10b3d3555b12c6ec458d76b16779845e5fc74609
LawrenceSTSHS <114170549+LawrenceSTSHS@users.noreply.github.com>
2025-08-07 22:58:20 -0700

    Sound asset and components

--- syrillian/src/engine/components/audio.rs
+++ syrillian/src/engine/components/audio.rs
@@ -1,3 +1,6 @@
+use kira::track::{SpatialTrackBuilder, SpatialTrackHandle};
+use nalgebra::{Quaternion, Vector3};
+use crate::assets::{Sound, H};
 use crate::components::Component;
 use crate::core::GameObjectId;
 use crate::World;
@@ -8,21 +11,18 @@ pub struct AudioReceiver {
 
 pub struct AudioEmitter {
     parent: GameObjectId,
-    sound_id: String,
+    handle: Option<H<Sound>>,
+    spatial_track: Option<SpatialTrackHandle>,
     playing: bool,
 }
 
 impl Component for AudioEmitter {
     fn new(parent: GameObjectId) -> Self {
-        Self { parent, sound_id: String::new(), playing: false }
+        Self { parent, handle: None, spatial_track: None, playing: false }
     }
 
     fn update(&mut self, world: &mut World) {
-        if !self.playing {
-            return;
-        }
-        let pos = self.parent.transform.position();
-        World::instance().audio_scene.play_sound(self.sound_id.as_str(), pos);
+
     }
 
     fn parent(&self) -> GameObjectId {
@@ -31,11 +31,17 @@ impl Component for AudioEmitter {
 }
 
 impl AudioEmitter {
-    pub fn set_sound(&mut self, sound: String) {
-        self.sound_id = sound;
+    pub fn init(&mut self, handle: H<Sound>, world: &mut World) {
+        self.handle = Some(handle);
+        let position = (Vector3::<f32>::new(0.0, 0.0, 0.0));
+        let orientation = Quaternion::<f32>::identity();
+        self.spatial_track = world.audio_scene.manager.add_spatial_sub_track(&world.audio_scene.listener, position, SpatialTrackBuilder::default()).ok();
     }
-    pub fn play(&mut self) {
+    pub fn play(&mut self, world: &mut World) {
+
         self.playing = true;
+        let track = &mut self.spatial_track;
+        world.audio_scene.play_sound(self.handle.unwrap(), &world.assets.clone(), self.parent.transform.position(), &mut self.spatial_track.as_mut().unwrap());
     }
 
     pub fn stop(&mut self) {
@@ -50,7 +56,7 @@ impl Component for AudioReceiver {
 
     fn update(&mut self, world: &mut World) {
         let transform = &self.parent().transform;
-        World::instance().audio_scene.set_receiver_position(transform.position());
+        World::instance().audio_scene.set_receiver_position(transform.position(), *transform.rotation());
     }
 
     fn parent(&self) -> GameObjectId {
