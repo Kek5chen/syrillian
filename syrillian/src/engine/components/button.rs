4cb163fad7438beac365f0313488aebb83b3542b
willow <git@willow.moe>
2025-12-20 13:58:26 +0100

    feat: ui button

---
+++ syrillian/src/engine/components/button.rs
@@ -0,0 +1,38 @@
+use crate::World;
+use crate::components::{Component, NewComponent};
+use crate::core::{EventType, GameObjectId};
+
+pub struct Button {
+    parent: GameObjectId,
+    click_handler: Vec<Box<dyn FnMut(&mut World) + 'static>>,
+}
+
+impl NewComponent for Button {
+    fn new(parent: GameObjectId) -> Self {
+        Self {
+            parent,
+            click_handler: Vec::new(),
+        }
+    }
+}
+
+impl Component for Button {
+    fn init(&mut self, world: &mut World) {
+        self.parent.notify_for(world, EventType::CLICK);
+    }
+
+    fn on_click(&mut self, world: &mut World) {
+        for handler in &mut self.click_handler {
+            handler(world);
+        }
+    }
+}
+
+impl Button {
+    pub fn add_click_handler<F>(&mut self, handler: F)
+    where
+        F: FnMut(&mut World) + 'static,
+    {
+        self.click_handler.push(Box::new(handler));
+    }
+}
